5af18b030015c482c63449403d0450bcba0a28e5
aditii3 <30756521+aditii3@users.noreply.github.com>
2017-08-15 18:28:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //new val set to main in div 
 var element = document.getElementById('main');
 element.innerHTML = 'new value being set';
+
+//move the image 
+var img = document.getElementById('dragon');
+img.onClick = function(){
+    img.style.marginLeft = "100px";
+} ;
