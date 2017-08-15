979b6002b96bca502819b3462d341821f0bc798f
aditii3 <30756521+aditii3@users.noreply.github.com>
2017-08-15 19:04:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML = 'new value being set';
 //move the image 
 var imge = document.getElementById('pic');
 imge.onClick = function(){
-    img.style.marginLeft = '100px';
+    imge.style.marginLeft = '100px';
 } ;
